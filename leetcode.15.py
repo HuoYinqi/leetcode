895d5590e527eebc1cf382ef8fceb0923717aaec
HuoYinqi <983673234@qq.com>
2020-12-25 13:36:05 +0800

    xxx

--- leetcode.15.py
+++ leetcode.15.py
@@ -1,11 +1,12 @@
-'''
+"""
 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
 注意：答案中不可以包含重复的三元组。
 示例：
 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
 满足要求的三元组集合为：
 [[-1, 0, 1], [-1, -1, 2]]
-'''
+"""
+
 
 class Solution:
     def three_sum(self, nums):
@@ -22,7 +23,7 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 i += 1
                 continue
-            
+
             left_id = i + 1
             right_id = size - 1
             target = -nums[i]
@@ -40,7 +41,7 @@ class Solution:
         return ret
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test = Solution()
-    ans = test.three_sum([-1, -1, 2, 0 ,1])
+    ans = test.three_sum([-1, -1, 2, 0, 1])
     print(ans)
--- leetcode.5.py
+++ leetcode.5.py
@@ -1,19 +1,20 @@
-'''
+"""
 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 示例 1：
 输入: "babad"
 输出: "bab"
 注意: "aba" 也是一个有效答案。
-'''
+"""
 # 采用动态数组的方法求解
 # dp[i][j]代表子串s[i:j+1]是否是回文子串 是个布尔值
+
 
 class Solution:
     def longest_palindrome(self, s):
         size = len(s)
         if size < 2:
             return s
-        
+
         ret = s[0]
         max_len = 1
         dp = [[False for _ in range(size)] for _ in range(size)]
@@ -23,14 +24,14 @@ class Solution:
                     if j - i < 3:
                         dp[i][j] = True
                     else:
-                        dp[i][j] = dp[i+1][j-1]
+                        dp[i][j] = dp[i + 1][j - 1]
                     if dp[i][j] and max_len < (j - i + 1):
                         max_len = j - i + 1
-                        ret = s[i:j+1]
+                        ret = s[i : j + 1]
         return ret
 
-if __name__ =='__main__':
-    test = Solution()
-    ans = test.longest_palindrome('gb')
-    print(ans)
 
+if __name__ == "__main__":
+    test = Solution()
+    ans = test.longest_palindrome("gb")
+    print(ans)
